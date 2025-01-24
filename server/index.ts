e2088e6658c05f73e6b195c7ca6dee27ef1d11e4
Isaaaya <snizhanna.bushko@gmail.com>
2025-01-24 11:07:11 +0200

    add export to the index.ts file

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,29 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const connectToDb = require("./db/connectToDb");
+const ApartmentRouter = require("./routes/apartment");
+
 const PORT = process.env.PORT || 3002;
-const cors = require('cors');
-
-const connectToDb = require('./db/connectToDb');
-const ApartmentRouter = require('./routes/apartment');
-
 const corsOptions = {
-    origin: 'http://localhost:3000',
-}
-
+  origin: "http://localhost:3000",
+};
 
 const start = () => {
-     connectToDb().
-     then(() => app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
-    }));
-}
+  connectToDb().then(() =>
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    })
+  );
+};
 
 start();
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
+app.use("/apartments", ApartmentRouter);
 
-app.use('/apartments', ApartmentRouter)
+
+module.exports = app;
+export default app;
